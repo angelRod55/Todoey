09a623a900ccd38ecd2fc22830a06791003d14c4
angel <angelrod@umich.edu>
2018-07-14 22:05:23 -0400

    MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Angel on 7/14/18.
+//  Copyright © 2018 VitreSoft. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
